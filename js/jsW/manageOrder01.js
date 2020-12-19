53b1771b890fb4b056ffeb7d8ff3e919fb7274f8
beiyan <758709763@qq.com>
2020-12-19 17:20:50 +0800

    完善订单显示demo

--- js/jsW/manageOrder01.js
+++ js/jsW/manageOrder01.js
@@ -1,17 +1,48 @@
 var app = new Vue({
     el:"#ordinaryContent",
     data:{
-        option:"测试",
+        option:"已完成",
+        orderListFlag:true,
         detailFlag:false,
+        codeDivFlag:false,
+        payDivFlag:false,
+        cancelDivFlag:false
     },
     methods:{
         //展示订单详情
-        detailShow:function () {
-            this.detailFlag = true;
+        detailShow:function (status) {
+            this.orderListFlag = false;
+            if(status == 1){
+                this.detailFlag = true;
+            }else if(status == 2){
+                this.detailFlag = true;
+                this.payDivFlag = true;
+                this.option = "待支付";
+            }else{
+                this.detailFlag = true;
+                this.codeDivFlag = true;
+                this.option = "待使用";
+            }
         },
         //关闭订单详情
         detailClose:function () {
+            this.orderListFlag = true;
             this.detailFlag = false;
+            this.codeDivFlag = false;
+            this.payDivFlag = false;
+        },
+        //展示取消订单
+        cancelShow:function () {
+            this.cancelDivFlag = true;
+            this.orderListFlag = false;
+            this.detailFlag = false;
+            this.codeDivFlag = false;
+            this.payDivFlag = false;
+        },
+        //关闭取消订单
+        cancelClose:function () {
+            this.cancelDivFlag = false;
+            this.orderListFlag = true;
         }
     }
 })
