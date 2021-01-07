57c23b9ed30fcbcae537963d0daffdcfd778dcdd
beiyan <758709763@qq.com>
2021-01-07 10:55:22 +0800

    加数据01

--- js/jsW/manageChat01.js
+++ js/jsW/manageChat01.js
@@ -3,6 +3,23 @@ var app = new Vue({
     data:{
         option:"测试",
         detailFlag:false,
+        //--------
+        //网关地址
+        chatIP:"http://localhost:5050/chat/",
+        //当前用户id
+        userId:2,
+        //分页
+        pageNo:1,
+        pageSize:6,
+        pages:1,
+        dateTotal:0,
+        navigatePageNums:[],
+        //数组
+        IdsList:[],
+        noViewCountList:[]
+    },
+    mounted:function(){
+        this.getIds(1);
     },
     methods:{
         //展示订单详情
@@ -12,6 +29,46 @@ var app = new Vue({
         //关闭订单详情
         detailClose:function () {
             this.detailFlag = false;
+        },
+        //-------------------
+        //根据自己的id，获取与自己有聊天记录的所有用户的id
+        getIds:function (pageNo) {
+            var that = this;
+            if(pageNo == null){
+                pageNo = that.pageNo;
+            }
+            if(pageNo < 1){
+                pageNo = that.pageNo;
+            }
+            if(pageNo > that.pages){
+                pageNo = that.pages;
+            }
+            axios.get(that.chatIP+"getOthersList?pageNo="+pageNo+"&pageSize="+that.pageSize+"&selfId="+that.userId).then(
+                function (value) {
+                    console.log(value.data.flag);
+                    //console.log(value.data.data.list);
+                    that.pageNo = pageNo;
+                    that.navigatePageNums = value.data.data.navigatepageNums;
+                    that.pages = value.data.data.pages;
+                    that.dateTotal = value.data.data.total;
+                    that.ordersList = value.data.data.list;
+                    //获取与各个用户的未读消息条数
+                    for(var i=0;i<that.ordersList.length;i++){
+                        that.getNoViewCounts(that.ordersList[i]);
+                    }
+                }
+            )
+        },
+        //未读消息条数
+        getNoViewCounts:function (otherId) {
+            var that = this;
+            axios.get(that.chatIP+"noViewCounts?otherId="+otherId+"&selfId="+that.userId).then(
+                function (value) {
+                    //console.log(value.data.flag);
+                    console.log(value.data.data);
+                    that.noViewCountList.push(value.data.data);
+                }
+            )
         }
     }
 })
--- js/jsW/manageOrder01.js
+++ js/jsW/manageOrder01.js
@@ -6,7 +6,26 @@ var app = new Vue({
         detailFlag:false,
         codeDivFlag:false,
         payDivFlag:false,
-        cancelDivFlag:false
+        cancelDivFlag:false,
+        //--------
+        //网关地址
+        orderIP:"http://localhost:5050/order/",
+        //当前用户id
+        userId:1,
+        //分页
+        pageNo:1,
+        pageSize:1,
+        pages:1,
+        dateTotal:0,
+        navigatePageNums:[],
+        //数组
+        ordersList:[],
+        landlordsList:[""],
+        //选中的订单index
+        pickOrderId:0
+    },
+    mounted:function(){
+        this.getOrders(1);
     },
     methods:{
         //展示订单详情
@@ -47,6 +66,39 @@ var app = new Vue({
         //订单详情页联系房东
         contactFD:function () {
             window.location.href = "manageChatTemp.html";
+        },
+        //--------------------------------------
+        //获取当前用户的订单
+        testfunction:function(){
+            console.log("testfunction");
+        },
+        getOrders:function (pageNo) {
+            var that = this;
+            if(pageNo == null){
+                pageNo = that.pageNo;
+            }
+            if(pageNo < 1){
+                pageNo = that.pageNo;
+            }
+            if(pageNo > that.pages){
+                pageNo = that.pages;
+            }
+            axios.get(that.orderIP+"getOrdersByUserIdOrderStatus?pageNo="+pageNo+"&pageSize="+that.pageSize+"&userId="+that.userId).then(
+                function (value) {
+                    console.log(value.data.flag);
+                    //console.log(value.data.data.navigatepageNums);
+                    that.pageNo = pageNo;
+                    that.navigatePageNums = value.data.data.navigatepageNums;
+                    that.pages = value.data.data.pages;
+                    that.dateTotal = value.data.data.total;
+                    that.ordersList = value.data.data.list;
+                }
+            )
+        },
+        pickOrder:function (index) {
+            var that = this;
+            that.pickOrderId = index;
         }
+
     }
 })
