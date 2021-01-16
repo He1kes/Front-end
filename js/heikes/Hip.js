88fb192d83b3a53783ebb4b6de1ebb7f91b926c1
Heikes <1192676603@qq.com>
2021-01-16 17:53:22 +0800

    房源搜索

---
+++ js/heikes/Hip.js
@@ -0,0 +1,4 @@
+var commentIp = "http://192.168.0.102:6606/consumer/comment";
+var houseIp = "http://192.168.0.102:6604/consumer/info";
+var typeIp = "http://192.168.0.102:6604/consumer/type";
+var picIp = "http://192.168.0.102:6604/consumer/pic";
